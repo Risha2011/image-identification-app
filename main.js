2402a279e5ce5da1cb55d3ae33bb77ac2c84e8c3
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2021-02-07 15:34:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,7 @@
-Webcam.set({
-    width:310,
-    height:300,
-    image_format : 'png',
-    png_quality:90,
-
-    constraints: {
-        facingMode: "environment"
-    }
-});
-
-camera = document.getElementById("camera");
-
-Webcam.attach('#camera');
-
-function take_snapshot(){
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
 }
 
-console.log('ml5 version:',ml5.version);
-
-classifier = ml5.imageClassifier('MobileNet',modelLoaded);
-
-function modelLoaded(){
-    console.log('Model Loaded !');
-}
-
-function check(){
-    img = document.getElementById('captured_image');
-    classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results){
-    if (error){
-        console.error(error);
-    }else {
-        console.log(results);
-        document.getElementById("object_name").innerHTML = results[0].label;
-    }
+function draw(){
 }
